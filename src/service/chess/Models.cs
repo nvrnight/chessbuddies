e06a9c7a4234cb2c577da7491eee2f469e6d73f1
jhastings <nvrnight@gmail.com>
2018-01-13 16:16:25 -0600

    Refactored models.

--- src/service/chess/Models.cs
+++ src/service/chess/Models.cs
@@ -1,5 +1,34 @@
+using System;
+using ChessDotNet;
+using Discord;
+using Newtonsoft.Json;
+
 namespace src
 {    
+    public class ChessMatch
+    {
+        public ChessGame Game {get; set;}
+        [JsonIgnore]
+        public IUser Challenger {get; set;}
+        [JsonIgnore]
+        public IUser Challenged {get; set;}
+        [JsonIgnore]
+        public IUser[] Players { get { return new[] { Challenger, Challenged }; } }
+        public ulong ChallengerId {get { return Challenger.Id; } }
+        public ulong ChallengedId {get { return Challenger.Id; } }
+        public ulong Channel {get; set;}
+    }
+    public class ChessChallenge
+    {
+        [JsonIgnore]
+        public IUser Challenger {get; set;}
+        [JsonIgnore]
+        public IUser Challenged {get; set;}
+        public ulong ChallengerId { get { return Challenger.Id; } }
+        public ulong ChallengedId { get { return Challenger.Id; } }
+        public ulong Channel {get; set;}
+        public DateTime ChallengeDate {get; set;}
+    }
     public class Position
     {
         public char X {get; set;}
--- src/service/chess/Services.cs
+++ src/service/chess/Services.cs
@@ -9,30 +9,6 @@ using Newtonsoft.Json;
 
 namespace src
 {
-    public class ChessChallenge
-    {
-        [JsonIgnore]
-        public IUser Challenger {get; set;}
-        [JsonIgnore]
-        public IUser Challenged {get; set;}
-        public ulong ChallengerId { get { return Challenger.Id; } }
-        public ulong ChallengedId { get { return Challenger.Id; } }
-        public ulong Channel {get; set;}
-        public DateTime ChallengeDate {get; set;}
-    }
-    public class ChessMatch
-    {
-        public ChessGame Game {get; set;}
-        [JsonIgnore]
-        public IUser Challenger {get; set;}
-        [JsonIgnore]
-        public IUser Challenged {get; set;}
-        [JsonIgnore]
-        public IUser[] Players { get { return new[] { Challenger, Challenged }; } }
-        public ulong ChallengerId {get { return Challenger.Id; } }
-        public ulong ChallengedId {get { return Challenger.Id; } }
-        public ulong Channel {get; set;}
-    }
     public interface IChessService
     {
         Task<ChessMove> Move(string rawMove);
