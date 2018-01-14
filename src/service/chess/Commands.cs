bb9fe9fcb743fde5af00c4663fae65345547311d
jhastings <nvrnight@gmail.com>
2018-01-13 23:45:00 -0600

    Added assets and proof of concept show command.

--- src/service/chess/Commands.cs
+++ src/service/chess/Commands.cs
@@ -1,18 +1,80 @@
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Discord.Commands;
 using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using SixLabors.Primitives;
 
 namespace src
 {
     public class ShowCommand : ModuleBase<SocketCommandContext>
     {
-        [Command("show")]
-        public async Task SayAsync()
+        private readonly IAssetService _assetService;
+
+        public ShowCommand(IAssetService assetService)
         {
-            await this.ReplyAsync($"Show command called.");
+            _assetService = assetService;
+        }
+
+        private void DrawPiece(IImageProcessingContext<Rgba32> processor, string name, int x, int y)
+        {
+            var pieceSquare = Image.Load(_assetService.GetImagePath($"{name}.png"));
+            processor.DrawImage(pieceSquare, new Size(50, 50), new Point(x * 50 + 117, y * 50 + 19), new GraphicsOptions());
+        }
+
+        [Command("show")]
+        public async Task SayAsync(string message = "")
+        {
+            using(var stream = new MemoryStream())
+            {
+                var board = Image.Load(_assetService.GetImagePath("board.png"));
+                
+                board.Mutate(processor => {
+                    DrawPiece(processor, "black_rook", 0, 0);
+                    DrawPiece(processor, "black_knight", 1, 0);
+                    DrawPiece(processor, "black_bishop", 2, 0);
+                    DrawPiece(processor, "black_queen", 3, 0);
+                    DrawPiece(processor, "black_king", 4, 0);
+                    DrawPiece(processor, "black_bishop", 5, 0);
+                    DrawPiece(processor, "black_knight", 6, 0);
+                    DrawPiece(processor, "black_rook", 7, 0);
+
+                    DrawPiece(processor, "black_pawn", 0, 1);
+                    DrawPiece(processor, "black_pawn", 1, 1);
+                    DrawPiece(processor, "black_pawn", 2, 1);
+                    DrawPiece(processor, "black_pawn", 3, 1);
+                    DrawPiece(processor, "black_pawn", 4, 1);
+                    DrawPiece(processor, "black_pawn", 5, 1);
+                    DrawPiece(processor, "black_pawn", 6, 1);
+                    DrawPiece(processor, "black_pawn", 7, 1);
+
+                    DrawPiece(processor, "white_pawn", 0, 6);
+                    DrawPiece(processor, "white_pawn", 1, 6);
+                    DrawPiece(processor, "white_pawn", 2, 6);
+                    DrawPiece(processor, "white_pawn", 3, 6);
+                    DrawPiece(processor, "white_pawn", 4, 6);
+                    DrawPiece(processor, "white_pawn", 5, 6);
+                    DrawPiece(processor, "white_pawn", 6, 6);
+                    DrawPiece(processor, "white_pawn", 7, 6);
+
+                    DrawPiece(processor, "white_rook", 0, 7);
+                    DrawPiece(processor, "white_knight", 1, 7);
+                    DrawPiece(processor, "white_bishop", 2, 7);
+                    DrawPiece(processor, "white_queen", 3, 7);
+                    DrawPiece(processor, "white_king", 4, 7);
+                    DrawPiece(processor, "white_bishop", 5, 7);
+                    DrawPiece(processor, "white_knight", 6, 7);
+                    DrawPiece(processor, "white_rook", 7, 7);                   
+                });
+
+                board.SaveAsPng(stream);
+                stream.Position = 0;
+                await this.Context.Channel.SendFileAsync(stream, "board.png");
+            }
+            
         }
     }
     public class RevertCommand : ModuleBase<SocketCommandContext>
