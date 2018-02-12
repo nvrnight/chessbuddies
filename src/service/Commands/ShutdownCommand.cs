d5d48d35f4b624af4dd4ea4d1d686b30ffba6e50
jhastings <nvrnight@gmail.com>
2018-02-12 09:01:49 -0600

    Changed the shutdown description. state.json is no longer used. Everything is database driven now.

--- src/service/Commands/ShutdownCommand.cs
+++ src/service/Commands/ShutdownCommand.cs
@@ -15,7 +15,7 @@ namespace ChessBuddies.Commands
         { }
         [IsAdmin]
         [Command("shutdown")]
-        [Summary("This will save on-going games to ./state.json and shutdown the bot.")]
+        [Summary("This will save on-going games and shutdown the bot.")]
         public Task Shutdown()
         {
             Program.ShutdownEvent.Set();
